6f4ea399ca0937a7d71792b58894db77ad7b97fa
JoFil24 <filipjoveski2407@gmail.com>
2025-09-18 10:14:02 +0200

    Added delete for points

--- F1UI/Views/js/Points.js
+++ F1UI/Views/js/Points.js
@@ -16,19 +16,8 @@
                 $(`#${points[entry]['driverId']}`).append(`<td>${points[entry]['driverName']}</td>`);
                 $(`#${points[entry]['driverId']}`).append(`<td>${points[entry]['points']}</td>`);
 
-                //list of parameters for the redirect
-                //var paramList = [
-                //    `raceId=${points[entry]['raceId']}`,
-                //    `driverId=${points[entry]['driverId']}`
-                //]
-
                 $(`#${points[entry]['driverId']}`).append(`<td><a onclick='redirectMainPage("updatePoints.html?raceId=${points[entry]['raceId']}&driverId=${points[entry]['driverId']}")'>Update</a></td>`);
-                //$(`#updateResultLinkForId${points[entry]['driverId']}`).click(function () {
-                //    multipleParamsRedirect('updatePoints.html', paramList);
-                //})
-                //                    attr('onclick', 'multipleParamsRedirect("updatePoints.html", "' + paramList + '")');
-
-                //$(`#${points[entry]['driverId']}`).append(`<td></td>`)
+                $(`#${points[entry]['driverId']}`).append(`<td><a onclick='deletePointsEntry(${getId()}, ${points[entry]['driverId']})'>Remove</a></td>`);
             })
         },
         error: function(xhr, status, error) {
@@ -62,4 +51,29 @@ function getOnePointsEntry() {
             $('#output').text('Error: ' + error);
         }
     })
+}
+
+function deletePointsEntry(raceId, driverId) {
+    if (confirm("Are you sure you want to delete this points entry?")){
+        try {
+            $.ajax({
+                url: `https://localhost:7253/api/Points/${driverId}/${raceId}`,
+                type: 'DELETE',
+                success: getSuccess,
+                error: getFail
+            });
+        }
+        catch (e) {
+            alert(e);
+        }
+        function getSuccess(data, textStatus, jqXHR) {
+            debugger;
+            alert(`Deleted points entry`);
+            redirectMainPage(`Points.html?id=${raceId}`);
+        };
+        function getFail(jqXHR, textStatus, errorThrown) {
+            debugger;
+            alert(jqXHR.status);
+        };
+    }
 }
